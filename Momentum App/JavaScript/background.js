0f254a78ce2883e728937b52706c52d134b31901
SungKim96 <120751395+SungKim96@users.noreply.github.com>
2023-10-03 22:12:43 -0400

    Create background.js

---
+++ Momentum App/JavaScript/background.js
@@ -0,0 +1,16 @@
+const images = [
+  'image1.jpeg',
+  'image2.jpeg',
+  'image3.jpeg',
+  'image4.jpeg',
+  'image5.jpeg',
+  'image6.jpeg',
+  'image7.jpeg',
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement('img');
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
