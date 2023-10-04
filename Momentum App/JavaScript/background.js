3d5d56c09fba6ddccc460954359a32bfa654eb96
SungKim96 <120751395+SungKim96@users.noreply.github.com>
2023-10-03 23:12:02 -0400

    Update background.js

--- Momentum App/JavaScript/background.js
+++ Momentum App/JavaScript/background.js
@@ -6,6 +6,19 @@ const images = [
   'image5.jpeg',
   'image6.jpeg',
   'image7.jpeg',
+  'image8.jpg',
+  'image9.jpg',
+  'image10.jpg',
+  'image11.jpg',
+  'image12.jpg',
+  'image13.jpg',
+  'image14.jpg',
+  'image15.jpg',
+  'image16.jpg',
+  'image17.jpg',
+  'image18.jpg',
+  'image19.jpg',
+  'image20.jpg',
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
